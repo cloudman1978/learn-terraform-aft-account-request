ead83bb868cd59b2c676bb5882efaa281900c51e
Adel SAFI <adel.safi.ext@orange.com>
2023-10-31 08:59:19 +0100

    Create sandbox-aftadel Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,39 @@ module "sandbox" {
 
   control_tower_parameters = {
     AccountEmail              = "contact+aftadel@ocbawspractice.com"
-    AccountName               = "sandbox-aft"
+    AccountName               = "sandbox-aftadel"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "adel.safi.ext@orange.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "contact+aft@ocbawspractice.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "adel.safi.ext@orange.com"
+    SSOUserFirstName          = "Sandbox2"
+    SSOUserLastName           = "AFT2"
   }
 
   account_tags = {
